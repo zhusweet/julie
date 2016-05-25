48b472a5d62b63d373ccb7dbc5be328e6491c695
朱鈺 / SHU，GYOKU <gyoku.shu.dh@hitachi-systems.com>
2016-05-25 16:58:10 +0900

    Add test1.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,8 @@
 class Startup {
     public static main(): number {
         console.log('Hello World, Julie,Tom,Daughter,Parents,Akemi');
+        
+        console.log('Welcom, Julie!');
         return 0;
     }
 }
